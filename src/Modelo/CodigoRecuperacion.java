245f3c618b5535224016a41cb949f60b65c340ad
Kevin <kevinjah1975@gmail.com>
2024-09-01 16:19:21 -0600

    Agregué validaciones a la pantalla del código de recuperación

--- src/Modelo/CodigoRecuperacion.java
+++ src/Modelo/CodigoRecuperacion.java
@@ -17,6 +17,16 @@ public class CodigoRecuperacion {
     
     public void verificarCodigo(String txtNum1, String txtNum2, String txtNum3, String txtNum4, String txtNum5, String txtNum6, jfrCodigoVerificacion vista){
         
+      if(txtNum1.isEmpty() || txtNum2.isEmpty() || txtNum3.isEmpty() || txtNum4.isEmpty() || txtNum5.isEmpty() || txtNum6.isEmpty()){
+        JOptionPane.showMessageDialog(vista, "Para actualizar la contraseña debe llenar todas las casillas del código de recuperación");
+        return;
+      }
+      
+      if(!txtNum1.matches("\\d") || !txtNum2.matches("\\d")  || !txtNum3.matches("\\d") || !txtNum4.matches("\\d") || !txtNum5.matches("\\d") || !txtNum6.matches("\\d")){
+       JOptionPane.showMessageDialog(vista, "Cada casilla del código de recuperación debe contener solo un número.");
+       return;
+      }
+        
       String codigoIngresado = txtNum1 + txtNum2 + txtNum3 + txtNum4 + txtNum5 + txtNum6;
       System.out.println("Código ingresado: " + codigoIngresado);
       System.out.println("Código esperado: " + CodigoRecuperacion.getCodigoRecuperacion());
