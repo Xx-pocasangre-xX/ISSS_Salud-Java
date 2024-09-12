8efd061f2455176b04e0610063090291b8d1db72
Dixi <20200201@ricaldone.edu.sv>
2024-09-11 21:21:20 -0600

    Diana estas loca

--- src/Controlador/ctrlPantallaLogin.java
+++ src/Controlador/ctrlPantallaLogin.java
@@ -3,6 +3,8 @@ package Controlador;
 import Modelo.Usuarios;
 import Vista.jfrPantallaLogin;
 import Vista.jfrPantallaMenuAdmin;
+import Vista.jfrPantallaMenuDoctor;
+import Vista.jfrPantallaMenuJefesEnfermeria;
 import Vista.jfrRecuperacionContra;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -32,23 +34,52 @@ public class ctrlPantallaLogin implements ActionListener{
              return;
            }
            
-           if(!correo.contains("@gmail") || !correo.contains(".com")){
-              JOptionPane.showMessageDialog(vista, "Correo no válido. Inténtelo de nuevo.");
-              return;
-           }
-           
            String contrasenaEncriptada = modelo.encryptPassword(contrasena);
            
-           if(modelo.validarCredenciales(correo, contrasenaEncriptada)){
-             JOptionPane.showMessageDialog(vista, "Login exitoso");
-             jfrPantallaMenuAdmin menuAdmin = new jfrPantallaMenuAdmin();
-             menuAdmin.setVisible(true);
-             menuAdmin.setLocationRelativeTo(null);
-             vista.dispose();
+           int idRol = modelo.validarCredenciales(correo, contrasenaEncriptada);
+           
+           if(idRol != -1){
+               switch (idRol){
+                   case 1: 
+                    JOptionPane.showMessageDialog(vista, "Login exitoso");
+                    jfrPantallaMenuAdmin menuAdmin = new jfrPantallaMenuAdmin();
+                    menuAdmin.setVisible(true);
+                    menuAdmin.setLocationRelativeTo(null);
+                    vista.dispose();
+                    break;
+                    
+                   case 2: 
+                    JOptionPane.showMessageDialog(vista, "La aplicación de escritorio es exclusiva para personal del ISSS. Por favor, descargue la versión móvil.");
+                    vista.txtEmail.setText("");
+                    vista.txtContrasena.setText("");
+                    break;
+                    
+                    case 3: 
+                    JOptionPane.showMessageDialog(vista, "Login exitoso");
+                    jfrPantallaMenuJefesEnfermeria menuJefesEnfermeria = new jfrPantallaMenuJefesEnfermeria();
+                    menuJefesEnfermeria.setVisible(true);
+                    menuJefesEnfermeria.setLocationRelativeTo(null);
+                    vista.dispose();
+                    break;
+                    
+                   default:
+                        JOptionPane.showMessageDialog(vista, "Credenciales incorrectas. Inténtalo de nuevo.");
+                        vista.txtEmail.setText("");
+                        vista.txtContrasena.setText("");
+                        break;
+               }
            }else{
-             JOptionPane.showMessageDialog(vista, "Credenciales incorrectas. Inténtalo de nuevo.");
-             vista.txtEmail.setText("");
-             vista.txtContrasena.setText("");
+             if (modelo.esDoctor(correo, contrasenaEncriptada)) {
+                    JOptionPane.showMessageDialog(vista, "Login exitoso");
+                    jfrPantallaMenuDoctor menuDoctor = new jfrPantallaMenuDoctor();
+                    menuDoctor.setVisible(true);
+                    menuDoctor.setLocationRelativeTo(null);
+                    vista.dispose();
+                } else {
+                    JOptionPane.showMessageDialog(vista, "Credenciales incorrectas. Inténtalo de nuevo.");
+                    vista.txtEmail.setText("");
+                    vista.txtContrasena.setText("");
+                }
            }
         }
         if(e.getSource() == vista.btnOlvidasteContra){
--- src/Modelo/Usuarios.java
+++ src/Modelo/Usuarios.java
@@ -6,22 +6,45 @@ import java.sql.*;
 
 public class Usuarios {
     
-    public boolean validarCredenciales(String correo, String contrasena){
-       String query = "SELECT COUNT(*) FROM Usuarios WHERE correo_electronico = ? AND contrasena = ?";
+    public int validarCredenciales(String correo, String contrasena){
+       String query = "SELECT id_rol FROM Usuarios WHERE correo_electronico = ? AND contrasena = ?";
        try(Connection conexion = ClaseConexion.getConexion();
           PreparedStatement pst = conexion.prepareStatement(query)){
          pst.setString(1, correo);
          pst.setString(2, contrasena);
          try(ResultSet rs = pst.executeQuery()){
            if(rs.next()){
-              return rs.getInt(1) > 0;
+              return rs.getInt("id_rol");
            }
          }
        }catch(SQLException e){
          e.printStackTrace();
        }
-       return false;
+       return -1;
        }
+    
+    public boolean esDoctor(String correo, String contrasena){
+      String query = "SELECT COUNT(*) FROM Doctores WHERE correo_doctor = ? AND contrasena_doctor = ?";
+      
+      System.out.println("Correo ingresado: " + correo);
+      System.out.println("Contraseña encriptada ingresada: " + contrasena);
+    
+      try(Connection conexion = ClaseConexion.getConexion();
+              PreparedStatement pst = conexion.prepareStatement(query)){
+        pst.setString(1, correo);
+        pst.setString(2, contrasena);
+        try(ResultSet rs = pst.executeQuery()){
+          if(rs.next()){
+              System.out.println("Consulta SQL ejecutada, valor devuelto: " + rs.getInt(1));
+            return rs.getInt(1) > 0;
+          }
+        }
+      }catch(SQLException e){
+           System.out.println("Error en la consulta SQL: " + e.getMessage());
+        e.printStackTrace();
+      }
+      return false;
+    }
     
     public String encryptPassword(String password){
       try{
